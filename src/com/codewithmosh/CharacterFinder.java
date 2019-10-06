52d40e8ee735f1d6366352ceaca4994fdb9a6f9e
Anita Virag <anitavirag.dev@gmail.com>
2019-10-06 21:00:38 +0200

    first repeating character

--- src/com/codewithmosh/CharacterFinder.java
+++ src/com/codewithmosh/CharacterFinder.java
@@ -1,7 +1,9 @@
 package com.codewithmosh;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class CharacterFinder {
     public char findFirstNonRepeatingChar(String str) {
@@ -17,6 +19,20 @@ public class CharacterFinder {
             if (map.get(ch) == 1) {
                 return ch;
             }
+        }
+
+        return Character.MIN_VALUE;
+    }
+
+    public char findFirstRepeatingChar(String str) {
+        Set<Character> set = new HashSet<>();
+
+        for (var ch : str.toCharArray()) {
+            if (set.contains(ch)) {
+                return ch;
+            }
+
+            set.add(ch);
         }
 
         return Character.MIN_VALUE;
--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         CharacterFinder finder = new CharacterFinder();
-        var ch = finder.findFirstNonRepeatingChar("a green apple");
+        var ch = finder.findFirstRepeatingChar("a green apple");
         System.out.println(ch);
     }
 }
