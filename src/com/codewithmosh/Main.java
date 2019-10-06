de700c271e0304f29a42c204e41b8c0afa65a4e8
Anita Virag <anitavirag.dev@gmail.com>
2019-10-06 14:30:35 +0200

    stack

---
+++ src/com/codewithmosh/Main.java
@@ -0,0 +1,16 @@
+package com.codewithmosh;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.pop();
+
+        System.out.println(stack);
+        System.out.println(stack.peek());
+    }
+}
