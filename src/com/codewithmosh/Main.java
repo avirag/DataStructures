9db1ba5e13a5b9dcf71cc0a0365dbfd74e8de613
Anita Virag <anitavirag.dev@gmail.com>
2019-10-06 17:30:14 +0200

    circular arrays

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,31 +1,22 @@
 package com.codewithmosh;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.Stack;
-
 public class Main {
 
     public static void main(String[] args) {
-        Queue<Integer> queue = new ArrayDeque<>();
-
-        queue.add(10);
-        queue.add(20);
-        queue.add(30);
+        ArrayQueue queue = new ArrayQueue(5);
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
         System.out.println(queue);
 
-        reverse(queue);
+        queue.dequeue();
+        queue.dequeue();
         System.out.println(queue);
-    }
 
-    public static void reverse(Queue<Integer> queue) {
-        Stack<Integer> stack = new Stack<>();
-        while(!queue.isEmpty()) {
-            stack.push(queue.remove());
-        }
+        queue.enqueue(50);
+        queue.enqueue(60);
 
-        while (!stack.isEmpty()) {
-            queue.add(stack.pop());
-        }
+        System.out.println(queue);
     }
 }
