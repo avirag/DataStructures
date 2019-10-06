b6938d642d2ff5d266f8bbe387925ccf7060a2ad
Anita Virag <anitavirag.dev@gmail.com>
2019-10-06 20:50:04 +0200

    first non-repeating character

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -3,12 +3,8 @@ package com.codewithmosh;
 public class Main {
 
     public static void main(String[] args) {
-        PriorityQueue queue = new PriorityQueue();
-        queue.add(2);
-        queue.add(7);
-        queue.add(9);
-        queue.add(1);
-
-        System.out.println(queue);
+        CharacterFinder finder = new CharacterFinder();
+        var ch = finder.findFirstNonRepeatingChar("a green apple");
+        System.out.println(ch);
     }
 }
