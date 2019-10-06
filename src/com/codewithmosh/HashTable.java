99283413e1ec85c6ddafe7875da593092c67c492
Anita Virag <anitavirag.dev@gmail.com>
2019-10-06 22:12:27 +0200

    refactoring hash table

--- src/com/codewithmosh/HashTable.java
+++ src/com/codewithmosh/HashTable.java
@@ -16,56 +16,55 @@ public class HashTable {
     private LinkedList<Entry>[] entries = new LinkedList[5];
 
     public void put(int key, String value) {
+        var entry = getEntry(key);
+        if (entry != null) {
+            entry.value = value;
+            return;
+        }
+
+        getOrCreateBucket(key).addLast(new Entry(key, value));
+    }
+
+    public void remove(int key) {
+        var entry = getEntry(key);
+        if (entry == null) {
+            throw new IllegalStateException();
+        }
+
+        getBucket(key).remove(entry);
+    }
+
+    public String get(int key) {
+        var entry = getEntry(key);
+        return entry == null ? null : entry.value;
+    }
+
+    private Entry getEntry(int key) {
+        var bucket = getBucket(key);
+
+        if (bucket != null) {
+            for (var entry : bucket) {
+                if (entry.key == key) {
+                    return entry;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private LinkedList<Entry> getOrCreateBucket(int key) {
         var index = hash(key);
 
         if (entries[index] == null) {
             entries[index] = new LinkedList<>();
         }
 
-        var bucket = entries[index];
-        for (var entry : bucket) {
-            if (entry.key == key) {
-                entry.value = value;
-                return;
-            }
-        }
-
-        bucket.addLast(new Entry(key, value));
+        return entries[index];
     }
 
-    public void remove(int key) {
-        var index = hash(key);
-        var bucket = entries[index];
-
-        if (bucket == null) {
-            throw new IllegalStateException();
-        }
-
-        for (var entry : bucket) {
-            if (entry.key == key) {
-                bucket.remove(entry);
-                return;
-            }
-        }
-
-        throw new IllegalStateException();
-    }
-
-    public String get(int key) {
-        var index = hash(key);
-        var bucket = entries[index];
-
-        if (bucket == null) {
-            return null;
-        }
-
-        for (var entry : bucket) {
-            if (entry.key == key) {
-                return entry.value;
-            }
-        }
-
-        return null;
+    private LinkedList<Entry> getBucket(int key) {
+        return entries[hash(key)];
     }
 
     private int hash(int key) {
